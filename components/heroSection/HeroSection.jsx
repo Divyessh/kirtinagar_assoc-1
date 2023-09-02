9fef1281b6df2418ff860fdde663e307f4829121
Mihir2324 <mihirraj444@gmail.com>
2023-09-02 13:45:39 +0530

    image update

--- components/heroSection/HeroSection.jsx
+++ components/heroSection/HeroSection.jsx
@@ -9,18 +9,18 @@ const HeroSection = () => {
     <div className="grid grid-rows-[600px,1fr] gap-4 h-[100%] md:h-screen md:grid-cols-8 pt-[30px] px-[22px] mb-3">
       <Image
         src={doodleBg}
-        alt="image"
-        width="100%"
-        height="100%"
+        alt="images"
         placeholder="empty"
+        height="100%"
+        width={2000}
+        objectFit="cover"
         priority
         style={{
           position: 'absolute',
           top: 0,
           left: 0,
           zIndex: 1,
-          height: '100vh',
-          objectFit: 'cover',
+          width: '2000px',
         }}
       />
       <SearchSection />
