bdc3aabe7145a04f77f81fa8319208f442dd01d8
Mihir2324 <mihirraj444@gmail.com>
2023-09-04 00:39:35 +0530

    added images

--- components/heroSection/SearchSection.jsx
+++ components/heroSection/SearchSection.jsx
@@ -5,7 +5,7 @@ import BgCarousel from './bgCarousel';
 
 const SearchSection = () => {
   return (
-    <div className="col-span-6 pb-[1px] rounded-[10px] h-[600px] px-[9px] flex items-end relative">
+    <div className="col-span-6 pb-[1px] rounded-[10px] h-[500px] md:h-[600px] px-[9px] flex items-end relative">
       <BgCarousel />
       <CategoryDropdown />
       <Socials />
--- components/heroSection/bgCarousel.jsx
+++ components/heroSection/bgCarousel.jsx
@@ -7,48 +7,55 @@ import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
 import heroBanner from '../../assets/png/heroBanner.png';
+import heroBanner2 from '../../assets/jpeg/heroBanner2.jpeg';
+import heroBanner3 from '../../assets/jpeg/heroBanner3.jpeg';
+import heroBanner4 from '../../assets/jpeg/heroBanner4.jpeg';
+import heroBanner5 from '../../assets/jpeg/heroBanner5.jpeg';
 
 const BgCarousel = () => {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const [emblaRef] = useEmblaCarousel({ loop: true, startIndex: 0 }, [Autoplay()]);
+  const imageArray = [
+    {
+      src: heroBanner,
+      id: 1,
+    },
+    {
+      src: heroBanner2,
+      id: 2,
+    },
+    {
+      src: heroBanner3,
+      id: 3,
+    },
+    {
+      src: heroBanner4,
+      id: 4,
+    },
+    {
+      src: heroBanner5,
+      id: 5,
+    },
+  ];
   return (
-    <div className="absolute rounded-[10px] top-0 left-0 z-1 w-[100%] h-[600px] embla" ref={emblaRef}>
+    <div className="absolute rounded-[10px] top-0 left-0 z-12 w-[100%] h-[500px] md:h-[600px] embla" ref={emblaRef}>
       <div className="relative embla__container">
-        <Image
-          src={heroBanner}
-          alt="image"
-          height="100%"
-          placeholder="blur"
-          priority
-          style={{
-            position: 'absolute',
-            borderRadius: '10px',
-            top: 0,
-            left: 0,
-            zIndex: 1,
-            height: '600px',
-            objectFit: 'cover',
-            width: '100%',
-          }}
-          className="embla__slide"
-        />
-        <Image
-          className="embla__slide"
-          src={heroBanner}
-          alt="image"
-          height="100%"
-          placeholder="blur"
-          priority
-          style={{
-            position: 'absolute',
-            borderRadius: '10px',
-            top: 0,
-            left: 0,
-            zIndex: 1,
-            height: '600px',
-            objectFit: 'cover',
-            width: '100%',
-          }}
-        />
+        {imageArray.map((image) => (
+          <Image
+            key={image.id}
+            src={image.src}
+            alt="image"
+            height="100%"
+            placeholder="blur"
+            priority
+            style={{
+              borderRadius: '10px',
+              zIndex: 20,
+              objectFit: 'cover',
+              width: '100%',
+            }}
+            className="embla__slide h-[500px] md:h-[600px]"
+          />
+        ))}
       </div>
     </div>
   );
