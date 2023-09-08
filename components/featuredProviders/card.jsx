c8e187bdfd5324d5896f03a8da75fe4b3b6af7f0
duplixx <duplixxx20@gmail.com>
2023-09-08 22:29:50 +0530

    card pagination added..

--- components/featuredProviders/card.jsx
+++ components/featuredProviders/card.jsx
@@ -5,11 +5,12 @@ import CardImg from '../../assets/png/cardimg.png';
 import services from '../../assets/svg/services.svg';
 import quote from '../../assets/svg/quote.svg';
 import line from '../../assets/svg/line.svg';
+import CardslideShow from './cardSwiper';
 
 const Card = () => (
-  <div className="card  md:w-[400px] w-[200px] bg-[#E5DFCF] shadow-2xl rounded-xl">
-    <Image src={Featured} alt="image" className="absolute top-14 left-0 " />
-    <Image src={CardImg} alt="image" className="rounded-xl" />
+  <div className="card  md:w-[350px] w-[200px] bg-[#E5DFCF] shadow-2xl rounded-xl">
+  <CardslideShow />
+  <Image src={Featured} alt="image" className="absolute top-14 left-0 " />
     <span className="absolute top-0 flex justify-between border-b-2 border-[#D9D9D9] w-full px-16  items-center">
       <div className="hover:cursor-pointer">
         <Image src={services} alt="image" className="pt-1 " />
--- components/featuredProviders/cardSwiper.jsx
+++ components/featuredProviders/cardSwiper.jsx
@@ -1,45 +1,91 @@
 'use client';
 
+import React,{ useCallback } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Swiper, SwiperSlide } from 'swiper/react';
+import useEmblaCarousel from 'embla-carousel-react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import 'swiper/css';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
-import CardImg from '../../assets/png/cardimg.png';
+import heroBanner from '../../assets/png/heroBanner.png';
+import heroBanner2 from '../../assets/jpeg/heroBanner2.jpeg';
+import heroBanner3 from '../../assets/jpeg/heroBanner3.jpeg';
+import heroBanner4 from '../../assets/jpeg/heroBanner4.jpeg';
+import heroBanner5 from '../../assets/jpeg/heroBanner5.jpeg';
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md';
 
-import Featured from '../../assets/svg/featured.svg';
+const BgCarousel = () => {
+  const [emblaRef,emblaApi] = useEmblaCarousel(
+    {
+      loop: true,
+      startIndex: 0,
+      autoplay: {
+        delay: 800, // Set the autoplay speed to 2 seconds (2000 milliseconds)
+        stopOnInteraction: true, // Stop autoplay on user interaction (optional)
+      },
+    },
+    [Autoplay()],
+  );
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
 
-const CardslideShow = () => (
-  <Swiper
-    spaceBetween={30}
-    autoplay={{
-      delay: 2500,
-      disableOnInteraction: false,
-    }}
-    Pagination
-    scrollbar={{ draggable: true }}
-    modules={[Autoplay, Pagination, Navigation]}
-    className=""
-  >
-    <SwiperSlide className="">
-      <Image src={Featured} alt="image" className="absolute top-12 left-0 " />
-      <Image src={CardImg} alt="image" />
-    </SwiperSlide>
-    <SwiperSlide className="">
-      <Image src={Featured} alt="image" className="absolute top-12 left-0 " />
-      <Image src={CardImg} alt="image" />
-    </SwiperSlide>
-    <SwiperSlide className="">
-      <Image src={Featured} alt="image" className="absolute top-12 left-0 " />
-      <Image src={CardImg} alt="image" />
-    </SwiperSlide>
-    <SwiperSlide className="">
-      <Image src={Featured} alt="image" className="absolute top-12 left-0 " />
-      <Image src={CardImg} alt="image" />
-    </SwiperSlide>
-  </Swiper>
-);
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+  const imageArray = [
+    {
+      src: heroBanner,
+      id: 1,
+    },
+    {
+      src: heroBanner2,
+      id: 2,
+    },
+    {
+      src: heroBanner3,
+      id: 3,
+    },
+    {
+      src: heroBanner4,
+      id: 4,
+    },
+    {
+      src: heroBanner5,
+      id: 5,
+    },
+  ];
+  return (
+    <div className=" embla" ref={emblaRef}>
+      <div className="relative h-[80%] embla__container">
+        {imageArray.map((image) => (
+          <Image
+            key={image.id}
+            src={image.src}
+            alt="image"
+            placeholder="blur"
+            loading="lazy"
+            style={{
+              borderRadius: '10px',
+              zIndex: 20,
+              objectFit: 'cover',
+              width: '419px',
+              height: '392px',
+              position: 'relative'
+            }}
+            className="embla__slide h-[500px] md:h-[600px]"
+          />
+        ))}
+      </div>
+      <div className="absolute flex justify-between top-[180px] w-full px-2">
+        <button className="embla__prev" onClick={scrollPrev} >
+          <MdArrowBackIos className="text-black text-4xl shadow-md rounded-full p-2 bg-primary backdrop-blur-md " />
+        </button>
+        <button className="embla__next" onClick={scrollNext}>
+          <MdArrowForwardIos className="text-black text-4xl shadow-md rounded-full p-2 bg-primary backdrop-blur-md" />
+        </button>
+    </div>
+    </div>
+  );
+};
 
-export default CardslideShow;
+export default BgCarousel;
