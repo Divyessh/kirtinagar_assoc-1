6382c2ec83ef28cd8981c1a18b2344c8ecb2cb99
Mihir2324 <mihirraj444@gmail.com>
2023-09-25 01:23:37 +0530

    ui changes

--- components/services/relatedProviders.jsx
+++ components/services/relatedProviders.jsx
@@ -6,7 +6,7 @@ const RelatedProviders = () => {
   return (
     <div>
       <Heading title="Related Providers" />
-      <div className="flex flex-col md:flex-row justify-between items-center my-[30px]">
+      <div className="flex flex-col md:flex-row justify-between items-center my-[30px] gap-[50px]">
         <Card />
         <Card />
         <Card />
