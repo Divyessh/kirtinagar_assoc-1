65382e405f005d8caef6a43f5b03788353c9e2c8
duplixx <duplixxx20@gmail.com>
2023-10-06 11:47:54 +0530

    minor

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -3,7 +3,6 @@ import galleryBanner from '../../assets/png/galleryImage.jpeg';
 import CardsSection from '../../components/galleryComponents/cardsSection';
 import PagesHeader from '../../components/galleryComponents/pagesheader';
 import PagitnationButton from '../../components/galleryComponents/paginationButton';
-import galleryBanner from '../../assets/png/galleryImage.jpeg';
 
 const GallerySection = () => {
   return (
