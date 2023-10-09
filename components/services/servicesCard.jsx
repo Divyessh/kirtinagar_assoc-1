0870935cfed75d814a9673adfdff923df1df966a
Mihir2423 <mihirraj444@gmail.com>
2023-10-10 03:27:23 +0530

    started services
    - create get users api
    - fetch data
    - integrate data
    - add crud functionality

--- components/services/servicesCard.jsx
+++ components/services/servicesCard.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Card from '../featuredProviders/card';
 
 const ServicesCard = () => {
@@ -5,7 +6,9 @@ const ServicesCard = () => {
     <div className="bg-white text-black flex-col w-full justify-center items-center text-center">
       <h1 className="mb-2 text-black  md:text-2xl text-md py-6">Featured Providers</h1>
       <div className="bg-white md:grid md:grid-cols-3 md:grid-rows-3 gap-2 gap-x-4 flex-4 px-8 ">
-        <Card />
+        <Link href="services/absd/about">
+          <Card />
+        </Link>
       </div>
     </div>
   );
