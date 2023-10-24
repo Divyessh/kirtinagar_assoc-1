1b01b78c88827f5c4b71dd95a97dad7866f5962f
Mihir2423 <mihirraj444@gmail.com>
2023-10-24 19:48:42 +0530

    fixed errors

--- components/galleryComponents/cardsSection.jsx
+++ components/galleryComponents/cardsSection.jsx
@@ -2,18 +2,18 @@ import GalleryCards from './galleryCard';
 
 const GallerySection = () => {
   return (
-    <div className='w-full flex justify-center items-center shadow-2xl shadow-gray-400'>
-    <div className="bg-white md:grid md:grid-cols-3 md:grid-rows-3 gap-y-2 shadow-lg  py-8 rounded-lg align-center justify-items-center px-12 md:w-[1400px]">
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-      <GalleryCards />
-    </div>
+    <div className="w-full flex justify-center items-center shadow-2xl shadow-gray-400">
+      <div className="bg-white md:grid md:grid-cols-3 md:grid-rows-3 gap-y-2 shadow-lg  py-8 rounded-lg align-center justify-items-center px-12 md:w-[1400px]">
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+        <GalleryCards />
+      </div>
     </div>
   );
 };
--- components/galleryComponents/pagesheader.jsx
+++ components/galleryComponents/pagesheader.jsx
@@ -4,12 +4,14 @@ import styles from '../page.module.css';
 
 const PagesHeader = ({ title, bannerImage }) => {
   return (
-    <header className={`${styles.header} flex items-center justify-center md:justify-between md:items-center w-full md:h-[46vh] md:px-2 md:pt-5 bg-primary `}>
+    <header
+      className={`${styles.header} flex items-center justify-center md:justify-between md:items-center w-full md:h-[46vh] md:px-2 md:pt-5 bg-primary `}
+    >
       <div className={` text-black md:text-4xl text-start h-full md:p-5 px-3 flex-col justify-start items-start space-y-4`}>
         <h1 className="mb-2 text-bold">{title}</h1>
         <div className="border-spacing-1 border-black border-2 md:block hidden " />
         <h1 className="text-semibold md:text-4xl md:pt-8 text-black">
-          KIRTI NAGAR TIMBER AND <br className='md:block hidden'/> FURNITURE MARKET
+          KIRTI NAGAR TIMBER AND <br className="md:block hidden" /> FURNITURE MARKET
         </h1>
       </div>
       <div className="md:h-[700px] w-[300px] md:w-[700px] mt-3">
