f1f75b0200e844f116406f830c9eac38899a7ffc
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2023-12-03 19:27:03 +0530

    Update page.jsx

--- app/ourteam/page.jsx
+++ app/ourteam/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PagesHeader from '../../components/galleryComponents/pagesheader';
 import galleryBanner from '../../assets/png/services.png';
-import TeamSection from '../../components/ourteam/TeamSection';
+// import TeamSection from '../../components/ourteam/TeamSection';
 import ExecMemebers from '../../components/ourteam/ExecMemebers';
-import WebCommitte from '../../components/ourteam/WebCommitte';
+// import WebCommitte from '../../components/ourteam/WebCommitte';
 
 const OurTeam = () => {
   return (
