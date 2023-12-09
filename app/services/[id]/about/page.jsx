10cd8ae2bb9ca22bb60790dc819017668727a749
Mihir2423 <mihirraj444@gmail.com>
2023-12-09 13:28:55 +0530

    fix: service page

--- app/services/[id]/about/page.jsx
+++ app/services/[id]/about/page.jsx
@@ -3,15 +3,15 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-// import Title from '../../../../components/services/title';
-// import ServiceWrapper from '../../../../components/services/serviceWrapper';
-// import Nav from '../../../../components/services/nav';
-// import AboutContent from '../../../../components/services/about/AboutContent';
-// import GalleryContent from '../../../../components/services/about/GalleryContent';
-// import Contact from '../../../../components/services/about/Contact';
-// import ServiceContent from '../../../../components/services/about/ServiceContent';
-// import ReqQuoteForm from '../../../../components/services/about/ReqQuoteForm';
-// import RelatedProviders from '../../../../components/services/relatedProviders';
+import Title from '../../../../components/services/title';
+import ServiceWrapper from '../../../../components/services/serviceWrapper';
+import Nav from '../../../../components/services/nav';
+import AboutContent from '../../../../components/services/about/AboutContent';
+import GalleryContent from '../../../../components/services/about/GalleryContent';
+import Contact from '../../../../components/services/about/Contact';
+import ServiceContent from '../../../../components/services/about/ServiceContent';
+import ReqQuoteForm from '../../../../components/services/about/ReqQuoteForm';
+import RelatedProviders from '../../../../components/services/relatedProviders';
 import SkeletonCard from '../../../../components/blogs/skeletonCard';
 
 const AboutServices = ({ params }) => {
@@ -27,17 +27,16 @@ const AboutServices = ({ params }) => {
   return isLoading ? (
     <SkeletonCard />
   ) : (
-    // <ServiceWrapper>
-    //   <Title id={id} />
-    //   <Nav pageName="About" id={id} />
-    //   <AboutContent id={id} />
-    //   <GalleryContent id={id} />
-    //   <Contact id={id} />
-    //   <ServiceContent id={id} title />
-    //   <ReqQuoteForm id={id} />
-    //   <RelatedProviders />
-    // </ServiceWrapper>
-    <h1 className="px-[20px] md:px-[100px] bg-white py-[10px]">Hello World</h1>
+    <ServiceWrapper>
+      <Title id={id} />
+      <Nav pageName="About" id={id} />
+      <AboutContent id={id} />
+      <GalleryContent id={id} />
+      <Contact id={id} />
+      <ServiceContent id={id} title />
+      <ReqQuoteForm id={id} />
+      <RelatedProviders />
+    </ServiceWrapper>
   );
 };
 
