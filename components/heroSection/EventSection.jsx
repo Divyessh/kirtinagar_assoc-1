ba19e9f93417a6c38b43686c3ad8ca09d0dca3d3
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-01-29 06:07:31 +0000

    ..

--- components/heroSection/EventSection.jsx
+++ components/heroSection/EventSection.jsx
@@ -6,7 +6,7 @@ const EventSection = () => {
     <div className="col-span-6 md:col-span-2 w-full bg-[#E5DFCF] h-[600px] md:h-[658px] rounded-[10px] py-[23px] px-[26px] relative ">
       <h1 className="text-[21px] font-bold text-[#413833] underline">Events and Blogs</h1>
       <div className="flex flex-col justify-between items-stretch gap-5 mt-4 overflow-hidden overflow-y-scroll md:max-h-[60vh] scrollbar">
-        <Link class="twitter-timeline" href="https://twitter.com/kirtinagarwhs?ref_src=twsrc%5Etfw">
+        <Link class="twitter-timeline" href="https://twitter.com/shreyansh487?ref_src=twsrc%5Etfw">
           Tweets by kirtinagarwhs
         </Link>{' '}
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
