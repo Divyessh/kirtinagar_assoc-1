24524accb5ba985eccf0d0388cc721d2071ea85c
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-01-29 18:09:39 +0000

    added

--- components/heroSection/EventSection.jsx
+++ components/heroSection/EventSection.jsx
@@ -5,8 +5,8 @@ const EventSection = () => {
   return (
     <div className="col-span-6 md:col-span-2 w-full bg-[#E5DFCF] h-[600px] md:h-[658px] rounded-[10px] py-[23px] px-[26px] relative ">
       <h1 className="text-[21px] font-bold text-[#413833] underline">Events and Blogs</h1>
-      <div className="flex flex-col justify-between items-stretch gap-5 mt-4 overflow-hidden overflow-y-scroll md:max-h-[60vh] scrollbar">
-        <Link class="twitter-timeline" href="https://twitter.com/kirtinagarwhs?ref_src=twsrc%5Etfw">  
+      <div className="flex flex-col justify-between items-stretch gap-5 mt-4 overflow-hidden overflow-y-scroll md:max-h-[60vh] scrollbar max-h-[50vh]">
+        <Link class="twitter-timeline" href="https://twitter.com/kirtinagarwhs?ref_src=twsrc%5Etfw">
           Tweets by kirtinagarwhs
         </Link>{' '}
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
