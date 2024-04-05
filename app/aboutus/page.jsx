330de7ed3afad6400aed1c293c1a3000c349bef5
duplixx <90516956+duplixx@users.noreply.github.com>
2024-04-05 22:17:12 +0530

    about us updated

--- app/aboutus/page.jsx
+++ app/aboutus/page.jsx
@@ -1,74 +1,99 @@
-import React from 'react';
-import Image from 'next/image';
-import PagesHeader from '../../components/galleryComponents/pagesheader';
-import aboutBanner from '../../assets/png/aboutusImage.jpeg';
-import aboutImage from '../../assets/png/Mask group (2).png';
-import styles from '../../components/page.module.css';
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import PagesHeader from "../../components/galleryComponents/pagesheader";
+import aboutBanner from "../../assets/png/aboutusImage.jpeg";
+import aboutImage from "../../assets/png/Mask group (2).png";
+import styles from "../../components/page.module.css";
 
 const AboutUsSection = () => {
   return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
       <PagesHeader title="About Us" bannerImage={aboutBanner} />
-      <main className="text-black md:text-xl text-md bg-primary px-12 py-6 flex  justify-items-center">
-        <span className="md:flex flex-cols space-x-5">
+      <main className="text-black md:text-xl text-md bg-primary md:px-12 px-6 py-6 flex justify-items-center">
+        <span className="md:flex flex-cols">
           <span className="w-[40%] ">
-            <Image src={aboutImage} width={527} height={378} alt="aboutImage" />
+            <Image
+              src={aboutImage}
+              width={527}
+              height={378}
+              alt="aboutImage"
+              className="md:h-full"
+            />
           </span>
           <span className="w-[56%]">
-            <h1 className="md:text-2xl text-lg font-bold mb-2">About Us</h1>
-            <p className="text-start">
-              Wood has been used as a building material for thousands of years. The chemical properties of wood are inherently complex, but
-              even in spite of this challenge, human beings have successfully harnessed the unique characteristics of wood to build a
-              seemingly unlimited variety of structures. Besides using this exceptionally versatile material for housing, it is used
-              extensively in furniture, home decor, handicrafts as well. It is remarkably strong in relation to its weight and highly
-              machinable. Wood is also the perfect example of an environmentally sustainable product, it is biodegradable and renewable and
-              carries the lowest carbon footprint in comparison to other available material. In addition, no high-energy fossil fuels are
-              required to produce wood, unlike other common materials such as steel or plastic.
+            <h1 className="md:text-2xl text-lg font-black mb-2">About Us</h1>
+            <p className="text-start text-md">
+              <b>KIRTI NAGAR, NEW DELHI, INDIA</b> <br />
+              (Hindi: कीर्ति नगर, Punjabi: विठडी ठगव, Urdu : كيرتى نگر) <br />{" "}
+              is an affluent area of West Delhi, India Postal code 110015.
+              <br /> Part of the Punjabi Bagh subzone of West Delhi, it
+              primarily a residential colony. Kirti Nagar is home to Asia's
+              largest Timber and furniture market SatGuru Ram Singh Marg
+              commonly known as RAMA ROAD housing showrooms of multi-national
+              home remodelling and fittings companies, the most reputed interior
+              design boutiques, imported kitchen and bath brands as well as
+              traditional carpentry furniture makers. <br /> <br />
+              Considered one of the upmarket areas in Delhi, the twin localities
+              of Mansarovar Garden Block B, C and Kirti Nagar BLOCK - A, B, 1, 2
+              & more indeed one of the posh areas in West Delhi. The colony
+              mostly consists of private houses and so called "Kothis", housing
+              rich Sikh businessmen and people who migrated from Pakistan after
+              partition constitute a significant part of the population. The
+              adjacent neighborhood of Rajouri Garden is a popular market,
+              comprising the Main Market, Nehru Market and many western style
+              indoor shopping malls including TDI Mall, Moments mall, TDI
+              Paragon Mall, Shoppers Stop, City Square, West Gate Mall, and
+              Paradise Mall. Together, this area is now the largest single
+              locality shopping conglomeration in Delhi ahead of South Delhi.
+              <br />
             </p>
           </span>
         </span>
       </main>
-      <main className="text-black md:text-xl text-md bg-primary px-12 py-6 ">
-        <span className="md:flex flex-cols space-x-5">
+      <main className="text-black md:text-xl text-md bg-primary md:px-12 px-6  py-6 ">
+        <span className="md:flex flex-cols space-x-10">
           <span className="w-[56%]">
-            <h1 className="md:text-2xl text-lg font-bold mb-2">Our Legacy</h1>
-            <p className="text-start mb-2">
-              Delhi is a unique historical city that has gone through Sea changes. Founded in the 11th century by Rajput Tomars, Muslim
-              Ruled from the 17th Century to 1857 and the British ruled from 1857 to 1947 when India achieved independence thereby Delhi
-              emerged as the National Capital of India. Delhi and New Delhi is a unique blend of tradition and modernity. Forts, Palaces,
-              Heritage Buildings, Tombs, etc. are the evidence of its roots in history. Skyscrapers, Malls, Metro Rail, flyovers, etc are
-              spread over all the corners of Delhi which have changed the face of Delhi. Another shot in the arm of Delhi was the
-              Commonwealth Games held in October 2010 which had benefited Delhi i.e. vast improvement of infrastructure, very wide roads,
-              integrated rails, bus transport, hospitality section, medical services, two new power plants, tourism, technologically
-              advanced buses.
+            <h1 className="md:text-2xl text-lg font-black mb-2">Demography</h1>
+            <p className={`text-start mb-2 space-y-4 text-md`}>
+              Kirti Nagar is well connected to the DELHI METRO via the KIRTI
+              NAGAR (Delhi Metro) station on PATEL ROAD and MAYAPURI (Delhi
+              Metro) station on RING ROAD (Mahatma Gandhi Marg) which are one of
+              the few Metro stations with a feeder bus service. Mansarovar
+              Garden and Kirti Nagar localities are adjacent to the Ring Road on
+              one side and Patel Road on the other which connect to Dhaula Kuan
+              and the Central Business District of Connaught Place,
+              respectively. Surrounding areas to Kirti Nagar are Mansarovar
+              Garden, Rajouri Garden, Punjabi Bagh, Saraswati Garden, Ramesh
+              Nagar and Patel Nagar, Loha Mandi NARAINA
             </p>
-            <button type="button" className=" btn bg-primary border-2  text-black hover:text-white rounded-lg mb-1">
-              Read more {'>'}
-            </button>
           </span>
           <span className="w-[40%] md:mt-0 mt-6 pt-2 md:pt-0">
             <Image src={aboutImage} width={527} height={378} alt="aboutImage" />
           </span>
         </span>
       </main>
-      <section className={`${styles.section3} w-full md:flex flex-cols md:block hidden justify-center items-center md:space-x-10 `}>
+      <section
+        className={`${styles.section3} w-full md:flex flex-cols  hidden justify-center items-center md:space-x-10 `}
+      >
         <div className="card rounded-br-full rounded-tl-full bg-white w-[574px] h-[181px] ">
           <text className="px-20 py-8 text-black">
-            Our Vision is that this market is developed as an ideal/popular market not only of India but of Asia for Timber and allied trade
+            Our Vision is that this market is developed as an ideal/popular
+            market not only of India but of Asia for Timber and allied trade
             while serving the nation with all humility.
           </text>
         </div>
         <div />
         <div className="card rounded-tr-full rounded-bl-full bg-white w-[574px] h-[181px]">
           <text className="px-16 py-8 text-black">
-            Our Vision is that this market is developed as an ideal/popular market not only of India but of Asia for Timber and allied trade
-            while serving the nation with all humility.
+            Our Mission is to take Kirtinagar to be recognized by international
+            markets and easily known as all in on place for furniture and needs
           </text>
         </div>
-        <div>wordl</div>
       </section>
     </>
   );
 };
+
 export default AboutUsSection;
--- components/footer.jsx
+++ components/footer.jsx
@@ -1,122 +1,161 @@
-import React from 'react';
-import Image from 'next/image'; // Use Image from next/image
-import Link from 'next/link';
-import { IoIosArrowForward } from 'react-icons/io';
-import members from '../assets/svg/Group 41members.svg';
-import welfare from '../assets/svg/Group 42.svg';
-import business from '../assets/svg/Group 43.svg';
-import location from '../assets/svg/location.svg';
+import React from "react";
+import Image from "next/image"; // Use Image from next/image
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
+import members from "../assets/svg/Group 41members.svg";
+import welfare from "../assets/svg/Group 42.svg";
+import business from "../assets/svg/Group 43.svg";
+import location from "../assets/svg/location.svg";
 
 const Footer = () => {
   const data = [
     {
       id: 1,
-      title: 'Members Directory',
+      title: "Members Directory",
       subtitle:
-        'This section provides information about individuals or entities in our directory. Find the right Taskers for your job based on their skills and expertise.',
+        "This section provides information about individuals or entities in our directory. Find the right Taskers for your job based on their skills and expertise.",
       icon: members,
     },
     {
       id: 2,
-      title: 'Members Welfare',
+      title: "Members Welfare",
       subtitle:
-        'Explore the well-being of our members. Learn about programs and initiatives designed to support the welfare of our community.',
+        "Explore the well-being of our members. Learn about programs and initiatives designed to support the welfare of our community.",
       icon: welfare,
     },
     {
       id: 3,
-      title: 'Members Business',
+      title: "Members Business",
       subtitle:
-        'Discover businesses associated with our members. Find the right services or products offered by businesses within our community.',
+        "Discover businesses associated with our members. Find the right services or products offered by businesses within our community.",
       icon: business,
     },
   ];
 
   return (
-    <div className="bg-[#413833] ">
-      <h1 className="text-white text-4xl font-bold text-center py-2 border-b-1 tracking-4">New Timber Market Dealer</h1>
-      <div className="md:flex ">
-        {data.map((item) => (
-          <div className="card p-8 text-white" key={item.id}>
-            <div className="flex items-center justify-center">
-              <span className="flex items-center justify-center bg-stone-200 w-[94px] h-[92px] rounded-full">
-                <Image src={item.icon} alt="members" className="object-cover" />
+    <>
+      <div className="relative  bg-white z-0">
+        <div className="w-full flex space-between ">
+          <Image
+            src="/footer.svg"
+            alt="footer"
+            width={233}
+            height={215}
+            className="absolute z-0 -top-14"
+          />
+          <Image
+            src="/footerright.svg"
+            alt="footer"
+            width={260}
+            height={215}
+            className="absolute  z-0 -top-16 right-0"
+          />
+        </div>
+
+        <div className="bg-[#413833]  absolute">
+          <h1 className="text-white text-4xl font-bold text-center py-2 border-b-1 tracking-4">
+            New Timber Market Dealer
+          </h1>
+          <div className="md:flex ">
+            {data.map((item) => (
+              <div className="card p-8 text-white" key={item.id}>
+                <div className="flex items-center justify-center">
+                  <span className="flex items-center justify-center bg-stone-200 w-[94px] h-[92px] rounded-full">
+                    <Image
+                      src={item.icon}
+                      alt="members"
+                      className="object-cover"
+                    />
+                  </span>
+                </div>
+                <div className="card-body flex-col justify-center items-center">
+                  <h1 className="text-xl text-white">{item.title}</h1>
+                  <p className="text-md text-[#DDC5C5] text-center px-8">
+                    {item.subtitle}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="md:flex flex-2 justify-evenly text-center items-center border-b-2 border-white space-y-2">
+            <div>
+              <span className="text-xl uppercase text-white text-center">
+                Service Provider
               </span>
+              <ul className="text-[#DDC5C5] text-center text-lg ">
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white " href="/">
+                    Branding
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    Design
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    Marketing
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    Advertisement
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="card-body flex-col justify-center items-center">
-              <h1 className="text-xl text-white">{item.title}</h1>
-              <p className="text-md text-[#DDC5C5] text-center px-8">{item.subtitle}</p>
+            <div>
+              <span className="text-xl text-white">USEFUL LINKS</span>
+              <ul className="text-[#DDC5C5] text-lg  text-center">
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    About us
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    Member Section
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:border-b-2 hover:text-white" href="/">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="">
+              <h1 className="text-xl text-center uppercase text-white ">
+                Location
+              </h1>
+              <Image
+                src={location}
+                alt="members"
+                className="object-cover p-3 w-full flex justify-center items-center"
+              />
+            </div>
+            <div className="space-y-3 text-white">
+              <span className="text-xl text-center uppercase">
+                SHOP LISTING
+              </span>
+              <h1 className="text-2xl">
+                Want the shop to be
+                <br />
+                listed?
+              </h1>
+              <button type="button" className="btn btn-primary rounded-full ">
+                See Pricing
+                <IoIosArrowForward />
+              </button>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="md:flex flex-2 justify-evenly text-center items-center border-b-2 border-white space-y-2">
-        <div>
-          <span className="text-xl uppercase text-white text-center">Service Provider</span>
-          <ul className="text-[#DDC5C5] text-center text-lg ">
-            <li>
-              <Link className="hover:border-b-2 hover:text-white " href="/">
-                Branding
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                Design
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                Marketing
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                Advertisement
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <span className="text-xl text-white">USEFUL LINKS</span>
-          <ul className="text-[#DDC5C5] text-lg  text-center">
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                About us
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                Member Section
-              </Link>
-            </li>
-            <li>
-              <Link className="hover:border-b-2 hover:text-white" href="/">
-                Contact Us
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="">
-          <h1 className="text-xl text-center uppercase text-white ">Location</h1>
-          <Image src={location} alt="members" className="object-cover p-3 w-full flex justify-center items-center" />
-        </div>
-        <div className="space-y-3 text-white">
-          <span className="text-xl text-center uppercase">SHOP LISTING</span>
-          <h1 className="text-2xl">
-            Want the shop to be
-            <br />
-            listed?
-          </h1>
-          <button type="button" className="btn btn-primary rounded-full ">
-            See Pricing
-            <IoIosArrowForward />
-          </button>
+          <div className="mx-2 text-center text-white">
+            Made with 🌌 by <Link href="$">VOID</Link>
+          </div>
         </div>
       </div>
-      <div className="mx-2 text-center text-white">
-        Made with 🌌 by <Link href="$">VOID</Link>
-      </div>
-    </div>
+    </>
   );
 };
 
