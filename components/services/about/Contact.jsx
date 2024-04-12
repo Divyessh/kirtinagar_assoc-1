34115e6840ed0f5dc4fcb7d7b0b5ccce7f33736b
Mihir2423 <mihirraj444@gmail.com>
2024-04-12 22:01:05 +0530

    fix build error

--- components/services/about/Contact.jsx
+++ components/services/about/Contact.jsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import React from "react";
-import axios from "axios";
-import { MdLocationOn } from "react-icons/md";
-import { BsFillTelephoneFill, BsChevronDown } from "react-icons/bs";
-import { FaEnvelope, FaRegEdit } from "react-icons/fa";
-import { useSession } from "next-auth/react";
-import { GiEarthAfricaEurope } from "react-icons/gi";
-import { AiFillFile, AiFillClockCircle } from "react-icons/ai";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import Heading from "./heading";
-import SkeletonCard from "../../blogs/skeletonCard";
-import { toast } from "react-toastify";
+import React from 'react';
+import axios from 'axios';
+import { MdLocationOn } from 'react-icons/md';
+import { BsFillTelephoneFill, BsChevronDown } from 'react-icons/bs';
+import { FaEnvelope, FaRegEdit } from 'react-icons/fa';
+import { useSession } from 'next-auth/react';
+import { GiEarthAfricaEurope } from 'react-icons/gi';
+import { AiFillFile, AiFillClockCircle } from 'react-icons/ai';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import Heading from './heading';
+import SkeletonCard from '../../blogs/skeletonCard';
 
 const Contact = ({ id }) => {
   const { data, isLoading } = useQuery({
-    queryKey: ["Provider", id],
+    queryKey: ['Provider', id],
     queryFn: async () => {
       const res = await axios.get(`/api/provider/${id}`);
       return res?.data?.data;
@@ -39,13 +39,13 @@ const Contact = ({ id }) => {
         return response.data;
       } catch (error) {
         // eslint-disable-next-line no-console
-        console.error("Something went wrong!", error);
-        throw new Error("Failed to add new student");
+        console.error('Something went wrong!', error);
+        throw new Error('Failed to add new student');
       }
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["Provider", id] });
-      toast.success("Details updated!");
+      queryClient.invalidateQueries({ queryKey: ['Provider', id] });
+      toast.success('Details updated!');
     },
     onError: (error) => {
       // eslint-disable-next-line no-console
@@ -60,168 +60,136 @@ const Contact = ({ id }) => {
     <SkeletonCard />
   ) : (
     <>
-      <div style={{ borderBottom: "1px solid #2B1607", paddingBottom: "16px" }}>
-        <div className="w-full flex justify-center items-center gap-2">
+      <div style={{ borderBottom: '1px solid #2B1607', paddingBottom: '16px' }}>
+        <div className="flex justify-center items-center gap-2 w-full">
           {isUser === id && (
             // eslint-disable-next-line jsx-a11y/label-has-associated-control
             <label htmlFor="my_modal_9" aria-label="open modal">
-              <FaRegEdit className="text-[20px] md:text-[38px] text-black md:mt-2" />
+              <FaRegEdit className="md:mt-2 text-[20px] text-black md:text-[38px]" />
             </label>
           )}
           <Heading title="Contact" />
         </div>
-        <div className="grid grid-cols-5 md:grid-cols-12 gap-[80px] mt-[40px]">
-          <div className="col-span-3 md:col-span-8 flex flex-col gap-[15px] md:gap-[30px]">
-            <div className="flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+        <div className="gap-[80px] grid grid-cols-5 md:grid-cols-12 mt-[40px]">
+          <div className="flex flex-col gap-[15px] md:gap-[30px] col-span-3 md:col-span-8">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px]">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <MdLocationOn className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-black text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px]">
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   {data?.address || contactInfo?.address}
                 </h1>
-                <h1 className="w-[80%] text-[13px] text-black md:text-[28px] font-[700] leading-[15px] md:leading-[40px]">
+                <h1 className="w-[80%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   GST No. 1234567890ABCD
                 </h1>
               </div>
             </div>
-            <div className="flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px]">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <BsFillTelephoneFill className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px] text-black">
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   {data?.contactNumber || contactInfo?.contactNumber}
                 </h1>
               </div>
             </div>
-            <div className="flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px]">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <GiEarthAfricaEurope className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px] text-black">
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   {data?.websiteLink || contactInfo?.websiteLink}
                 </h1>
               </div>
             </div>
-            <div className="flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px]">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <FaEnvelope className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px] text-black">
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   Request a quote
                 </h1>
               </div>
             </div>
-            <div className="max-md:ml-1  flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px] max-md:ml-1">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <AiFillFile className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-[13px] text-black md:text-[28px] font-[700] leading-[15px] md:leading-[40px]">
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">
                   {data?.email}
                 </h1>
               </div>
             </div>
-            <div className="flex items-start justify-center gap-[10px] md:gap-[20px]">
-              <div className="w-[5%] text-[13px] md:text-[30px] mt-[0px] md:mt-[7px]">
+            <div className="flex justify-center items-start gap-[10px] md:gap-[20px]">
+              <div className="mt-[0px] md:mt-[7px] w-[5%] text-[13px] md:text-[30px]">
                 <AiFillClockCircle className="text-black" />
               </div>
               <div className="w-[95%]">
-                <h1 className="w-[100%] text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px] text-black">
-                  Timings
-                </h1>
+                <h1 className="w-[100%] font-[700] text-[13px] text-black md:text-[28px] leading-[15px] md:leading-[40px]">Timings</h1>
               </div>
             </div>
           </div>
-          <div className="col-span-2 md:col-span-4 text-[13px] md:text-[28px] font-[700] leading-[15px] md:leading-[40px] text-center">
+          <div className="col-span-2 md:col-span-4 font-[700] text-[13px] text-center md:text-[28px] leading-[15px] md:leading-[40px]">
             <h1 className="text-black">LOCATION</h1>
-            <iframe
-              src={data?.location}
-              className="h-full w-full"
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
+            <iframe title="con" src={data?.location} className="w-full h-full" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
           </div>
         </div>
-        <div className="text-center flex items-center justify-center gap-2">
-          <h1 className="font-[600] text-[15px] md:text-[23px] text-black">
-            9 AM - 9 PM
-          </h1>
-          <button
-            type="button"
-            className=" w-fit rounded-full p-[3px] bg-[#382F2A]"
-            aria-label="Time"
-          >
+        <div className="flex justify-center items-center gap-2 text-center">
+          <h1 className="font-[600] text-[15px] text-black md:text-[23px]">9 AM - 9 PM</h1>
+          <button type="button" className="bg-[#382F2A] p-[3px] rounded-full w-fit" aria-label="Time">
             <BsChevronDown className="font-[600] text-[8px] text-white md:text-[16px]" />
           </button>
         </div>
       </div>
       <input type="checkbox" id="my_modal_9" className="modal-toggle" />
       <div className="modal" role="dialog">
-        <div className="modal-box bg-white text-black flex flex-col gap-4">
-          <h3 className="text-lg font-bold">Edit Contact Details!</h3>
+        <div className="flex flex-col gap-4 bg-white text-black modal-box">
+          <h3 className="font-bold text-lg">Edit Contact Details!</h3>
           <input
             type="text"
             placeholder="Enter Address"
-            className="border p-2 rounded-md outline-black"
+            className="p-2 border rounded-md outline-black"
             defaultValue={contactInfo?.address || data?.address}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, address: e.target.value })
-            }
+            onChange={(e) => setContactInfo({ ...contactInfo, address: e.target.value })}
           />
           <input
             type="text"
             placeholder="Enter Contact Number"
-            className="border p-2 rounded-md outline-black"
+            className="p-2 border rounded-md outline-black"
             defaultValue={contactInfo?.contactNumber || data?.contactNumber}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, contactNumber: e.target.value })
-            }
+            onChange={(e) => setContactInfo({ ...contactInfo, contactNumber: e.target.value })}
           />
           <input
             type="text"
             placeholder="Enter WebSite Link"
-            className="border p-2 rounded-md outline-black"
+            className="p-2 border rounded-md outline-black"
             defaultValue={contactInfo?.websiteLink || data?.websiteLink}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, websiteLink: e.target.value })
-            }
+            onChange={(e) => setContactInfo({ ...contactInfo, websiteLink: e.target.value })}
           />
           <input
             type="text"
             placeholder="Enter Email"
-            className="border p-2 rounded-md outline-black"
+            className="p-2 border rounded-md outline-black"
             defaultValue={contactInfo?.email || data?.email}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, email: e.target.value })
-            }
+            onChange={(e) => setContactInfo({ ...contactInfo, email: e.target.value })}
           />
           <input
             type="text"
             placeholder="Enter Google Maps Location"
-            className="border p-2 rounded-md outline-black"
+            className="p-2 border rounded-md outline-black"
             defaultValue={contactInfo?.location || data?.location}
-            onChange={(e) =>
-              setContactInfo({ ...contactInfo, location: e.target.value })
-            }
+            onChange={(e) => setContactInfo({ ...contactInfo, location: e.target.value })}
           />
-          <button
-            type="button"
-            aria-label="Save Info"
-            onClick={handleSave}
-            className="bg-[#FF9800] text-black p-2 rounded-lg w-fit"
-          >
+          <button type="button" aria-label="Save Info" onClick={handleSave} className="bg-[#FF9800] p-2 rounded-lg w-fit text-black">
             Save
           </button>
         </div>
-        <label
-          className="modal-backdrop"
-          htmlFor="my_modal_9"
-          aria-label="modalClose"
-        >
+        <label className="modal-backdrop" htmlFor="my_modal_9" aria-label="modalClose">
           <input type="checkbox" id="my_modal_7" className="modal-toggle" />
           Close
         </label>
